f6c29c7e9b57fa025a33bef3e7bfbe3946931fbf
Elena Voloshina <elen3011@yandex.ru>
2020-07-04 20:30:13 +0300

    в js добавленн код закрытия popup  по Сохранить

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,12 @@
-const openPopupButton = document.querySelector('.profile__edit');
-const popup = document.querySelector('.popup');
-const closePopupButton = document.querySelector('.popup__close');
-const formProfile = popup.querySelector('.form');
-const nameInput = popup.querySelector('.form__input_type_name');
-const activityInput = popup.querySelector('.form__input_type_activity');
-const profileName = popup.querySelector('.profile__name');
-const profileActivity = popup.querySelector('.profile__activity');
+let openPopupButton = document.querySelector('.profile__edit');
+let popup = document.querySelector('.popup');
+let closePopupButton = popup.querySelector('.popup__close');
+let formProfile = popup.querySelector('.form');
+let nameInput = popup.querySelector('.form__input_type_name');
+let activityInput = popup.querySelector('.form__input_type_activity');
+let profileName = document.querySelector('.profile__name');
+let profileActivity = document.querySelector('.profile__activity');
+let saveProfile = popup.querySelector('form__button');
 
 function togglePopup() {
     popup.classList.toggle('popup_opened');
@@ -15,10 +16,13 @@ openPopupButton.addEventListener('click', togglePopup);
 
 closePopupButton.addEventListener('click', togglePopup);
 
+
 function formSubmitHandler (evt) {
     evt.preventDefault();
     profileName.textContent = nameInput.value;
     profileActivity.textContent = activityInput.value;
+    popup.classList.remove('popup_opened');
+
 }
 
 formProfile.addEventListener('submit', formSubmitHandler);
