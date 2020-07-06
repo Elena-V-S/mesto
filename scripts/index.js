7f7a0b5eee2abb93828cfe46cb9d8cb445cb018f
Elena Voloshina <elen3011@yandex.ru>
2020-07-06 08:49:30 +0300

    исправлены замечания ревью 1

--- scripts/index.js
+++ scripts/index.js
@@ -9,20 +9,17 @@ let profileActivity = document.querySelector('.profile__activity');
 let saveProfile = popup.querySelector('form__button');
 
 function togglePopup() {
+    nameInput.value = profileName.textContent;
+    activityInput.value = profileActivity.textContent;
     popup.classList.toggle('popup_opened');
 }
-
-openPopupButton.addEventListener('click', togglePopup);
-
-closePopupButton.addEventListener('click', togglePopup);
-
 
 function formSubmitHandler (evt) {
     evt.preventDefault();
     profileName.textContent = nameInput.value;
     profileActivity.textContent = activityInput.value;
-    popup.classList.remove('popup_opened');
-
+    togglePopup();
 }
-
+openPopupButton.addEventListener('click', togglePopup);
+closePopupButton.addEventListener('click', togglePopup);
 formProfile.addEventListener('submit', formSubmitHandler);
